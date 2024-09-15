97a423f0a336abcfa7e45d5f3602827b5036fb64
Gheorghe Georgescu <gggscu@gmail.com>
2024-09-15 01:43:11 -0400

    signed commit

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,10 @@
 import { Box } from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} 1hq. All Rights Reserved.
+      &copy; {new Date().getFullYear()} <Link href='https://www.github.com/gscu'>gscu</Link>. All Rights Reserved.
     </Box>
   )
 }
